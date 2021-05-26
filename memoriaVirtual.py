29856a14678128da89ce2a720866e9457eb5f69b
JaimeMontemayor <jaimemontemayor97@gmail.com>
2021-05-26 10:19:19 -0500

    Esquema de memoria

---
+++ memoriaVirtual.py
@@ -0,0 +1,29 @@
+# ------------------------------------------------------------
+# Federico Alcerreca Treviño - A01281459
+# Jaime Andres Montemayor Molina - A01176573
+# ------------------------------------------------------------
+#Direcciones Globales
+gI = 2000
+gF = 4000
+gC = 6000
+#Temporales Globales
+gtI = 8000
+gtF = 10000
+gtC = 12000
+gtB = 14000
+#Constantes
+gcI = 16000
+gcF = 18000
+gcC = 20000
+gcS = 22000
+gcB = 24000
+
+#Direcciones Locales
+lI = 26000
+lF = 28000
+lC = 30000
+#Temporales Locales
+ltI = 32000
+ltF = 34000
+ltC = 36000
+ltB = 38000
